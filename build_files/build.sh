7333053fec24aaaae3a6c98e0c3f28458d161ec7
Atmois <info@atmois.com>
2025-10-03 21:52:17 +0100

    Remove direct packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 set -ouex pipefail
 
-# Prework
-rm -rf /opt/Filen/
-
 # Packages
 dnf5 -y config-manager setopt "terra".enabled=true
 terraPackages=(
@@ -11,17 +8,8 @@ terraPackages=(
     "feishin"
 )
 
-wget -O Filen_linux_x86_64.rpm https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm
-direct_packages=(
-    "./Filen_linux_x86_64.rpm"
-)
-
 packages=(
     ${terraPackages[@]}
-    ${direct_packages[@]}
 )
 
 dnf install -y ${packages[@]}
-
-# Cleanup
-rm -f Filen_linux_x86_64.rpm
