be6459c137374ae6acb616620faab1cdc76974b8
Atmois <info@atmois.com>
2025-10-10 22:49:05 +0100

    Add musescore + muse sounds manager packages

--- build_files/build.sh
+++ build_files/build.sh
@@ -2,14 +2,27 @@
 set -ouex pipefail
 
 # Packages
+
+# https://packages.fedoraproject.org
+fedoraPackages=(
+    "musescore"
+)
+
+# https://terra.fyralabs.com
 dnf5 -y config-manager setopt "terra".enabled=true
 terraPackages=(
     "vesktop"
     "feishin"
 )
 
+customPackages=(
+    "https://muse-cdn.com/Muse_Sounds_Manager_x64.rpm"
+)
+
 packages=(
     ${terraPackages[@]}
+    ${customPackages[@]}
+    ${fedoraPackages[@]}
 )
 
 dnf install -y ${packages[@]}
