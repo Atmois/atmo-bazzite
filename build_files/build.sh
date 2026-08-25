eec387b28a7482a1564acddfcd763becd54f8671
Atmois <info@atmois.com>
2026-08-25 21:28:11 +0100

    try a fix

--- build_files/build.sh
+++ build_files/build.sh
@@ -46,10 +46,10 @@ terraPackages=(
 externalPackages=(
     "https://cdn.filen.io/@filen/desktop/release/latest/Filen_linux_x86_64.rpm"
     "https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-x86_64.rpm"
-    "$(curl -s https://api.github.com/repos/ONLYOFFICE/DesktopEditors/releases/latest | grep -oP '"browser_download_url": "\K[^"]*desktopeditors\.x86_64\.rpm')"
+    "https://github.com/ONLYOFFICE/DesktopEditors/releases/latest/download/onlyoffice-desktopeditors.x86_64.rpm"
     "https://vivaldi.com/download/vivaldi-stable.x86_64.rpm"
-    #"$(curl -s https://api.github.com/repos/TriliumNext/Trilium/releases/latest | grep -oP '"browser_download_url": "\K[^"]*linux-x64\.rpm')"
 )
+#"$(curl -s https://api.github.com/repos/TriliumNext/Trilium/releases/latest | grep -oP '"browser_download_url": "\K[^"]*linux-x64\.rpm')"
 
 # Ensure /opt is a real directory for RPM installation
 [ -L /opt ] && rm -f /opt
